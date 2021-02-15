350965d3e48818dc2ade17e33e3e0a54d4a7cb75
James Post <avilapost93@gmail.com>
2021-02-15 11:54:42 -0800

    .post

--- src/Components/NewBeer.js
+++ src/Components/NewBeer.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 function NewBeer(props) {
     return (
+
+        //I know i need to use post. here..... but I couldn't get it to work with online resources.
         <div>
             NewBeer
         </div>
